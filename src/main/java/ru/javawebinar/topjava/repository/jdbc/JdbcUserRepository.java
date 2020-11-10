2a0dff7e5c24eab9d7d8d5d50ff0fff736fe0b5d
Евгений <catshaurma@gmail.com>
2020-11-10 22:16:02 +0300

    HW06 Optional (point 2.3) Implementation

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
@@ -4,21 +4,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
-import java.util.List;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
 
 @Repository
 @Transactional(readOnly = true)
 public class JdbcUserRepository implements UserRepository {
 
     private static final BeanPropertyRowMapper<User> ROW_MAPPER = BeanPropertyRowMapper.newInstance(User.class);
+
+    /**
+     * Special Extractors for extracting users with their roles from resultSet
+     * after sql-request of type "user JOIN INNER user_roles"
+     */
+    private static final ResultSetExtractor<Map<String, User>> RESULT_SET_EXTRACTOR =
+            resultSet -> getMapForExtractor(resultSet, "id");
+
+    private static final ResultSetExtractor<Map<String, User>> RESULT_SET_EXTRACTOR_WITH_EMAIL =
+            resultSet -> getMapForExtractor(resultSet, "email");
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -61,19 +75,50 @@ public class JdbcUserRepository implements UserRepository {
 
     @Override
     public User get(int id) {
-        List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE id=?", ROW_MAPPER, id);
-        return DataAccessUtils.singleResult(users);
+        Map<String, User> users = jdbcTemplate.query(
+                "SELECT * FROM users u INNER JOIN user_roles ur ON u.id=ur.user_id AND u.id=?",
+                RESULT_SET_EXTRACTOR, id);
+
+        return users != null ? users.get(String.valueOf(id)) : null;
     }
 
     @Override
     public User getByEmail(String email) {
-//        return jdbcTemplate.queryForObject("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
-        List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
-        return DataAccessUtils.singleResult(users);
+        Map<String, User> users = jdbcTemplate.query(
+                "SELECT * FROM users u INNER JOIN user_roles ur ON u.id = ur.user_id WHERE u.email=? ORDER BY u.name, u.email",
+                RESULT_SET_EXTRACTOR_WITH_EMAIL, email);
+
+        return users != null ? users.get(email) : null;
     }
 
     @Override
     public List<User> getAll() {
-        return jdbcTemplate.query("SELECT * FROM users ORDER BY name, email", ROW_MAPPER);
+        Map<String, User> users = jdbcTemplate.query(
+                "SELECT * FROM users u INNER JOIN user_roles ur ON u.id = ur.user_id ORDER BY u.name, u.email",
+                RESULT_SET_EXTRACTOR);
+
+        return users != null && users.size() > 0 ? new ArrayList<>(users.values()) : Collections.emptyList();
+    }
+
+    private static <T> Map<T, User> getMapForExtractor(ResultSet rs, String keyColumn) throws SQLException {
+        Map<T, User> users = new HashMap<>();
+        while (rs.next()) {
+            String key = rs.getString(keyColumn);
+            if (users.containsKey(key)) {
+                users.get(key).getRoles().add(Role.valueOf(rs.getString("role")));
+            } else {
+                users.put((T) key,
+                        new User(
+                                rs.getInt("id"),
+                                rs.getString("name"),
+                                rs.getString("email"),
+                                rs.getString("password"),
+                                rs.getInt("calories_per_day"),
+                                rs.getBoolean("enabled"),
+                                rs.getDate("registered"),
+                                Set.of(Role.valueOf(rs.getString("role")))));
+            }
+        }
+        return users;
     }
 }
